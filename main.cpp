37e5ff493a07a6b23caa1534300f46c6ded2407b
Cole Ferguson <coleferguson9@gmail.com>
2020-11-30 17:27:27 -0800

    added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#define SENSOR_PIN  D2
+
+int sensorVal = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(SENSOR_PIN, INPUT); //Sensor input
+
+//   WiFi.begin("DANGER","TRASH0707");
+//   Serial.print("Connecting...");
+
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     delay(500);
+//     Serial.print(".");
+//   }
+//   Serial.println();
+
+//   Serial.print("Connected, IP address: ");
+//   Serial.println(WiFi.localIP());
+ }
+
+void loop() {
+  sensorVal = digitalRead(SENSOR_PIN);
+
+  if (sensorVal == HIGH){
+    Serial.println("MOTION DETECTED");
+    delay(500);
+  }
+
+  else if (sensorVal == LOW){
+    Serial.println("Nothing...");
+    delay(500);
+  }
+}
