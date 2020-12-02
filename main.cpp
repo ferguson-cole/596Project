0a868bf0d07449443834d3c40b793b6432c2ecc1
Cole Ferguson <coleferguson9@gmail.com>
2020-12-01 16:06:55 -0800

    added example to send sms

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Arduino.h"
-#include <ESP8266WiFi.h>
+#include "ESP8266WiFi.h"
+#include <iostream>
+#include <textmagic.h>
 
 #define SENSOR_PIN  D2
 
@@ -35,6 +37,37 @@ void loop() {
     Serial.println("Nothing...");
     delay(500);
   }
+}
+
+void sendNotification() {
+  if (argc < 3) {
+    std::cout << "********* Please enter valid login & api key *********" << std::endl;
+    std::cout << ">> bin/messages_example username C7XDKZOQZo6HvhJPtO0MBcWl3qwtp2" << std::endl;
+    return 1;
+  }
+
+  std::cout << "********* MessagesController example *********" << std::endl;
+  Rest::RequestData vars;
+  Client tm(argv[1], argv[2]);
+  //******************* Send example ***********************
+  std::cout << "********* MessagesController.send() *********" << std::endl;
+  vars["phones"] = "999";
+  vars["text"] = "The simple fake message";
+  SendResultModel result = tm.Messages().send(vars);
+  if (tm.Messages().isError){
+    std::cout
+      << tm.Messages().lastError.code << " * "
+      << tm.Messages().lastError.message << " * "
+      << std::endl;
+    return 1;
+  } else {
+    std::cout
+       << result.href << " * "
+       << result.type  << " * "
+       << result.messageId  << " * "
+       << std::endl;
+  }
+
 }
 
 
